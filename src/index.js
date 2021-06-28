f24326ce13ac63fb3fa4ba42f34164589ccd0635
Antonio Hernández Castilleja <antonio.hernandezcastilleja@outlook.com>
2021-06-27 19:21:51 -0500

    Board's render modified II.

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ function Square(props) {
 
 class Board extends React.Component {
     handleClick(i) {
-        const squares = this.state.squares.slice();
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
+            history: history.concat([{
             squares: squares,
+            }]),
             xIsNext: !this.state.xIsNext,
         });
     }
